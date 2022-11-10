bf35290b0d6cacf9d3c68950b36e90c0469a0113
Pysio <71202163+pysio2007@users.noreply.github.com>
2022-11-10 07:03:47 +0000

    3.0.6

--- src/main/java/com/greencat/Antimony.java
+++ src/main/java/com/greencat/Antimony.java
@@ -54,7 +54,7 @@ import java.util.HashMap;
 public class Antimony {
     public static final String MODID = "antimony";
     public static final String NAME = "Antimony-Client";
-    public static final String VERSION = "3.0.5";
+    public static final String VERSION = "3.0.6";
     private static final String Sb = "Sb";
 
     public static float strafe;
