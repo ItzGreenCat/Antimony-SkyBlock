8be5d8458033797e1665a83b168af68f43ece18a
Pysio <71202163+pysio2007@users.noreply.github.com>
2022-11-26 02:40:45 +0800

    Update AntimonyChannel.java

--- src/main/java/com/greencat/antimony/common/Chat/AntimonyChannel.java
+++ src/main/java/com/greencat/antimony/common/Chat/AntimonyChannel.java
@@ -7,7 +7,7 @@ public class AntimonyChannel {
     public static Socket socket;
     public AntimonyChannel() {
         try {
-            socket = new Socket(InetAddress.getByName("antimonychat.greencatmc.top")/*"127.0.0.1"*/, 11451);
+            socket = new Socket(InetAddress.getByName("amc.pysio.online")/*"127.0.0.1"*/, 11451);
             socket.setOOBInline(true);
         } catch (Exception e) {
             e.printStackTrace();
